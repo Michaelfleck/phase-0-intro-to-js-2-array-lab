0ac5bd6f99f49a1f50d9a928e289ab592c555db8
mfleck <michael.j.fleck@gmail.com>
2021-09-29 17:01:19 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+    //return cats;
+}
+
+
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+    //return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+var cat1 = [];
+function appendCat() {
+    cat1 = [...cats ,"Broom"];
+    return cat1;
+}
+
+var cat2 = [];
+function prependCat() {
+    cat2 = ["Arnold", ...cats];
+    return cat2;
+}
+
+function removeLastCat() {
+    const newcats = cats.slice(0, cats.length -1);
+    return newcats;
+}
+
+function removeFirstCat() {
+    const newcats = cats.slice(1)
+    return newcats;
+    return cats;
+}
+
+
+
